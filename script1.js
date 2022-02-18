7ef175bb4f7c5f6af24083b2722b5ba3bd4f7193
Dev Nasser <93291368+Dev-Nasser@users.noreply.github.com>
2022-02-18 12:44:25 -0300

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,13 +1,16 @@
-const nome = document.getElementById("nome").value
-const idade = document.getElementById("Idade").value
-const  linguagem = document.getElementById("linguagem").value
+function mostrar(){
 
-function mostrar (){
+    let idade = document.getElementById("Idade").value
+    let nome = document.getElementById("nome").value
+    let  linguagem = document.getElementById("linguagem").value
 
-    alert(`Olá vejo que o seu nome é ${nome}, sua idade é de ${idade} anos e ja esta aprendendo ${linguagem}!`)
+    alert(`Olá vejo que o seu nome e ${nome}, sua idade é de ${idade} anos e ja esta aprendendo ${linguagem}!`);
+    if (idade > 18){
+        alert ('Sua verificacao de idade nos mostra que você já é maior de 18');
+     
+    }
+
 
 }
-
-
 
 
